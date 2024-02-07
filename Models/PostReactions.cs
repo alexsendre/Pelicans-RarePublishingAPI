6e0d52640865190ba76167c3881ca865714bfa34
Alex Sendre <rares.sendre@gmail.com>
2024-02-06 19:11:32 -0600

    add comments, post reactions, and reactions classes

---
+++ Models/PostReactions.cs
@@ -0,0 +1,10 @@
+﻿namespace Pelicans_RarePublishing.Models
+{
+    public class PostReactions
+    {
+        public int Id { get; set; }
+        public int ReactionId { get; set; }
+        public int UserId { get; set; }
+        public int PostId { get; set; }
+    }
+}
