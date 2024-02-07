2bf0733be3a2c638694d757361a743a7ba8aa686
B33blebroxx <b33blebroxx@gmail.com>
2024-02-06 20:14:14 -0600

    Created mock data for posts

--- Program.cs
+++ Program.cs
@@ -1,3 +1,69 @@
+using Pelicans_RarePublishing.Models;
+
+List<Posts> posts = new()
+{
+    new Posts
+    {
+       Id = 61-8009667,
+       UserId = 39-6187989,
+       CategoryId = 04-5762968,
+       Title = "Discovery",
+       PublicationDate = new DateTime (2023,08,28),
+       ImageUrl = "http://dummyimage.com/155x100.png/ff4444/ffffff",
+       Content = "Nunc rhoncus dui vel sem.",
+       Approved = true
+    },
+
+     new Posts
+    {
+       Id = 70-5141389,
+       UserId = 19-0919972,
+       CategoryId = 92-4674448,
+       Title = "Mandatory",
+       PublicationDate = new DateTime (2023,11,11),
+       ImageUrl = "http://dummyimage.com/250x100.png/cc0000/ffffff",
+       Content = "Proin eu mi. Nulla ac enim.",
+       Approved = false
+    },
+
+      new Posts
+    {
+       Id = 18-2496682,
+       UserId = 23-1158461,
+       CategoryId = 11-245876,
+       Title = "Scary",
+       PublicationDate = new DateTime (2024,01,10),
+       ImageUrl = "http://dummyimage.com/250x100.png/cc0000/ffffff",
+       Content = "Praesent blandit lacinia erat.",
+       Approved = true
+    },
+
+      new Posts
+    {
+       Id = 37-3151614,
+       UserId = 77-1808556,
+       CategoryId = 73-8851661,
+       Title = "Re-Engineered",
+       PublicationDate = new DateTime (2023,04,20),
+       ImageUrl = "http://dummyimage.com/220x100.png/5fa2dd/ffffff",
+       Content = "Etiam pretium iaculis justo.",
+       Approved = true
+    },
+
+      new Posts
+    {
+       Id = 42-4242424,
+       UserId = 32-4567812,
+       CategoryId = 12-3456789,
+       Title = "Engineered",
+       PublicationDate = new DateTime (2023,06,11),
+       ImageUrl = "http://dummyimage.com/220x100.png/5fa2dd/ffffff",
+       Content = "Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis.",
+       Approved = true
+    },
+
+};
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
