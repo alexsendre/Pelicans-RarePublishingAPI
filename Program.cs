a33b16e62225a17caf8ba90f378a2b9ebccf59cb
Alex Sendre <rares.sendre@gmail.com>
2024-02-08 18:25:37 -0600

    finish list for post reactions

--- Program.cs
+++ Program.cs
@@ -321,6 +321,45 @@ List<PostTags> postTags = new()
     }
 };
 
+List<PostReactions> postReactions = new()
+{
+    new PostReactions
+    {
+        Id = 1,
+        ReactionId = 1,
+        UserId = 2,
+        PostId = 1
+    },
+    new PostReactions
+    {
+        Id = 2,
+        ReactionId= 2,
+        UserId = 3,
+        PostId = 2,
+    },
+    new PostReactions
+    {
+        Id = 3,
+        ReactionId = 1,
+        UserId= 4,
+        PostId= 3,
+    },
+    new PostReactions
+    {
+        Id = 4,
+        ReactionId = 1,
+        UserId = 5,
+        PostId= 4,
+    },
+    new PostReactions
+    {
+        Id = 5,
+        ReactionId = 2,
+        UserId = 1,
+        PostId = 5
+    }
+};
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
